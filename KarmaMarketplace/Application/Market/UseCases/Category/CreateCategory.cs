a7f811b88b93bd7f18012a6350cfd0fbbcfe4b16
pikoUsername <galymzhan.amantaj@gmail.com>
2024-03-27 23:37:09 +0500

    Remove unused code

--- KarmaMarketplace/Application/Market/UseCases/Category/CreateCategory.cs
+++ KarmaMarketplace/Application/Market/UseCases/Category/CreateCategory.cs
@@ -1,6 +1,7 @@
 ﻿using KarmaMarketplace.Application.Common.Interactors;
 using KarmaMarketplace.Application.Common.Interfaces;
 using KarmaMarketplace.Application.Market.Dto;
+using KarmaMarketplace.Application.Market.Utils;
 using KarmaMarketplace.Domain.Market.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,48 +21,13 @@ namespace KarmaMarketplace.Application.Market.Interactors.Category
         {
             await _accessPolicy.FailIfNoAccess(Domain.User.Enums.UserRoles.Admin);
 
-            List<OptionEntity> Options = []; 
-
-            foreach (var option in dto.Options)
-            {
-                if (option.Type == Domain.Market.Enums.OptionTypes.Selector) {
-                    Guard.Against.Null(option.Group, message: "Group for option is not provided"); 
-
-                    Options.Add(OptionEntity.CreateSelector(
-                        value: option.Value,
-                        label: option.Label,
-                        group: option.Group,
-                        field: option.Field,
-                        sequence: option.Sequence)); 
-                } 
-                if (option.Type == Domain.Market.Enums.OptionTypes.Switch)
-                {
-                    Options.Add(OptionEntity.CreateSwitch(
-                        label: option.Label,
-                        field: option.Field,
-                        sequence: option.Sequence)); 
-                }
-                if (option.Type == Domain.Market.Enums.OptionTypes.Range) {
-                    Guard.Against.Null(option.ValueRangeMin, message: "Value range min is null");
-                    Guard.Against.Null(option.ValueRangeMax, message: "Value range max is null");
-
-                    Options.Add(OptionEntity.CreateRange(
-                        label: option.Label,
-                        value: option.Value,
-                        field: option.Field,
-                        sequence: option.Sequence,
-                        min: (int)option.ValueRangeMin,
-                        max: (int)option.ValueRangeMax)); 
-                }
-            }
-
             var game = await _context.Games.FirstOrDefaultAsync(x => x.Id == dto.GameId);
 
             Guard.Against.Null(game, "Game does not exists"); 
 
             var newCategory = CategoryEntity.Create(
                 name: dto.Name,
-                options: Options, 
+                options: OptionFactory.CreateOptions(dto.Options), 
                 gameId: dto.GameId, 
                 slug: null); 
 
