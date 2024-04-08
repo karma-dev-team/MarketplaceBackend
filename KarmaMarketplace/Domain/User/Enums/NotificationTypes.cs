61d496c25e410bb6a117bc065b13b7dd606a910a
pikoUsername <galymzhan.amantaj@gmail.com>
2024-04-08 11:04:36 +0500

    Small change for proper parsing

--- KarmaMarketplace/Domain/User/Enums/NotificationTypes.cs
+++ KarmaMarketplace/Domain/User/Enums/NotificationTypes.cs
@@ -1,5 +1,8 @@
-﻿namespace KarmaMarketplace.Domain.User.Enums
+﻿using System.Text.Json.Serialization;
+
+namespace KarmaMarketplace.Domain.User.Enums
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum NotificationTypes
     {
         Other, 
